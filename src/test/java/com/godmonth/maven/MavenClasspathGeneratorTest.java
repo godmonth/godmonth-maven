31e5bbebd80ed49e54f00b8851721cd1d1bb511d
godmonth <link2ibm@126.com>
2013-08-09 14:17:21 +0800

    commit

---
+++ src/test/java/com/godmonth/maven/MavenClasspathGeneratorTest.java
@@ -0,0 +1,20 @@
+package com.godmonth.maven;
+
+import org.eclipse.aether.collection.DependencyCollectionException;
+import org.eclipse.aether.resolution.DependencyResolutionException;
+import org.testng.annotations.Test;
+
+import com.godmonth.maven.MavenClasspathGenerator;
+
+public class MavenClasspathGeneratorTest {
+
+	@Test(enabled = true)
+	public void test() throws DependencyCollectionException, DependencyResolutionException {
+		MavenClasspathGenerator mcg = new MavenClasspathGenerator();
+		mcg.setLocalRepoPath("target/local-repo");
+		mcg.setRemoteRepoUrl("http://nexus.weihui.com:8081/content/groups/public");
+		mcg.init();
+		System.out.println(mcg.getJarFileList("org.apache.commons:commons-lang3:3.1"));
+		System.out.println(mcg.getJarFileList("com.fasterxml.jackson.core:jackson-databind:2.2.1"));
+	}
+}
